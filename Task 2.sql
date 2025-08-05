065b175af96b39defebe9b33c61121ba9c349fd3
suyog deshmukh <vikrantd240802@gmail.com>
2025-08-05 20:28:21 +0530

    Add files via upload

---
+++ Task 2.sql
@@ -0,0 +1,26 @@
+USE student1;
+
+CREATE TABLE student2 (
+    ID INT (10),
+    Name VARCHAR (50),
+    Age INT (2),
+    Grade Varchar (10) );
+
+INSERT INTO student2 (ID, Name, Age, Grade)
+VALUES (1, 'Suyog', 21, 'A');
+
+INSERT INTO student2 (ID, Name, Age)
+VALUES (2, 'Vikrant', 22);
+
+
+INSERT INTO student2 (ID, Name, Age, Grade)
+VALUES (3, 'Anjali', NULL, 'B');
+
+INSERT INTO student2 (ID, Name)
+VALUES (4, 'Ravi');
+
+
+
+
+
+
